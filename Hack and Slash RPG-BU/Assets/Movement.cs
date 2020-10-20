06c625beb8801d4aa90cc438a45c2765eaef7209
Yash Patel <yspatel97@gmail.com>
2020-10-20 05:34:01 -0400

    Player Movement on click
    - added click movement (replaced file ThirdPersonMovement.cs with Movement.cs)
    
    - Added smooth player follow Camera
    
    - dropped CineMachaine Cam

---
+++ Hack and Slash RPG-BU/Assets/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private NavMeshAgent navMesh;
+
+    private bool moving = false;
+
+    private string groundTag = "Ground";
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                navMesh.destination = hit.point;
+            }
+        }
+
+        if (navMesh.remainingDistance <= navMesh.stoppingDistance)
+        {
+            moving = false;
+        }
+        else
+        {
+            moving = true;
+        }
+
+        animator.SetBool("moving", moving);
+    }
+}
---
+++ Hack and Slash RPG-BU/Assets/PlayerFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothFactor = 0.125f;
+
+    public Vector3 offset;
+
+    // LateUpdate is called after Update methods
+    void LateUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothFactor);
+        transform.position = smoothPos;
+    }
+}
