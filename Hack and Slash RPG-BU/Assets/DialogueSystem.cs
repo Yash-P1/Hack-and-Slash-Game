dd4f2cf2f06283908abcb1867c79cd49f40bc795
Tatvagna Shah <shahtatvagna@gmail.com>
2020-10-20 21:23:57 -0400

    NPC behaviour
    - Goldbloom character added
    - animation added
    - dialogues added

---
+++ Hack and Slash RPG-BU/Assets/DialogueSystem.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueSystem: MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public GameObject dialogueGUI;
+    public Transform dialogueBoxGUI;
+
+    public float letterDelay = 0.1f;
+    public float letterMultiplier = 0.5f;
+
+    public KeyCode DialogueInput = KeyCode.F;
+
+    public string Names;
+
+    public string[] dialogueLines;
+
+    public bool letterIsMultiplied = false;
+    public bool dialogueActive = false;
+    public bool dialogueEnded = false;
+    public bool outOfRange = true;
+
+    public AudioClip audioClip;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        dialogueText.text = "";
+        nameText.text = "";
+        dialogueBoxGUI.gameObject.SetActive(false);
+        //dialogueGUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        //if(!outOfRange){
+            dialogueGUI.SetActive(false);
+        
+    }
+
+    public void EnterRangeOfNPC()
+    {
+        outOfRange = false;
+        dialogueGUI.SetActive(true);
+        if (dialogueActive == true)
+        {
+            dialogueGUI.SetActive(false);
+        }
+    }
+
+    public void NPCName()
+    {
+        outOfRange = false;
+        dialogueBoxGUI.gameObject.SetActive(true);
+        nameText.text = Names;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!dialogueActive)
+            {
+                dialogueActive = true;
+                StartCoroutine(StartDialogue());
+            }
+        }
+        StartDialogue();
+    }
+
+    private IEnumerator StartDialogue()
+    {
+        if (outOfRange == false)
+        {
+            int dialogueLength = dialogueLines.Length;
+            int currentDialogueIndex = 0;
+
+            while (currentDialogueIndex < dialogueLength || !letterIsMultiplied)
+            {
+                if (!letterIsMultiplied)
+                {
+                    letterIsMultiplied = true;
+                    StartCoroutine(DisplayString(dialogueLines[currentDialogueIndex++]));
+
+                    if (currentDialogueIndex >= dialogueLength)
+                    {
+                        dialogueEnded = true;
+                    }
+                }
+                yield return 0;
+            }
+
+            while (true)
+            {
+                if (Input.GetKeyDown(DialogueInput) && dialogueEnded == false)
+                {
+                    break;
+                }
+                yield return 0;
+            }
+            dialogueEnded = false;
+            dialogueActive = false;
+            DropDialogue();
+        }
+    }
+
+    private IEnumerator DisplayString(string stringToDisplay)
+    {
+        if (outOfRange == false)
+        {
+            int stringLength = stringToDisplay.Length;
+            int currentCharacterIndex = 0;
+
+            dialogueText.text = "";
+
+            while (currentCharacterIndex < stringLength)
+            {
+                dialogueText.text += stringToDisplay[currentCharacterIndex];
+                currentCharacterIndex++;
+
+                if (currentCharacterIndex < stringLength)
+                {
+                    if (Input.GetKey(DialogueInput))
+                    {
+                        yield return new WaitForSeconds(letterDelay * letterMultiplier);
+
+                        if (audioClip) audioSource.PlayOneShot(audioClip, 0.5F);
+                    }
+                    else
+                    {
+                        yield return new WaitForSeconds(letterDelay);
+
+                        if (audioClip) audioSource.PlayOneShot(audioClip, 0.5F);
+                    }
+                }
+                else
+                {
+                    dialogueEnded = false;
+                    break;
+                }
+            }
+            while (true)
+            {
+                if (Input.GetKeyDown(DialogueInput))
+                {
+                    break;
+                }
+                yield return 0;
+            }
+            dialogueEnded = false;
+            letterIsMultiplied = false;
+            dialogueText.text = "";
+        }
+    }
+
+    public void DropDialogue()
+    {       
+        dialogueGUI.SetActive(false);
+        dialogueBoxGUI.gameObject.SetActive(false);
+    }
+
+    public void OutOfRange()
+    {
+        outOfRange = true;
+        if (outOfRange == true)
+        {
+            letterIsMultiplied = false;
+            dialogueActive = false;
+            StopAllCoroutines();
+            dialogueGUI.SetActive(false);
+            dialogueBoxGUI.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Hack and Slash RPG-BU/Assets/Goldbloom_behaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goldbloom_behaviour : MonoBehaviour
+{
+    private Transform player;
+    private float dist;
+    public float movespeed;
+    public float howclose;
+
+    public Animator animator;
+    private bool near = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player tag should be "Player" or else change the tag in line below
+        player = GameObject.FindGameObjectWithTag("Player").transform; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(player.position, transform.position);
+
+        if(dist <= howclose){
+            transform.LookAt(player);
+            //GetComponent<Rigidbody>().AddForce(transform.forward * movespeed);
+            near = true;
+        }else{
+            near = false;
+        }
+        animator.SetBool("isNear", near);
+        
+
+        //for melee attach or explode
+        if(dist <= 1.5f){
+            //make godbloom do things
+        }
+    }
+}
